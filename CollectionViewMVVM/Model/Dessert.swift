b8a645efdea16fdd13fb98ab00ef13bd41b9178d
Adriana <melina.gzz@hotmail.com>
2021-02-03 08:13:09 -0800

    Update code

---
+++ CollectionViewMVVM/Model/Dessert.swift
@@ -0,0 +1,9 @@
+//
+//  Dessert.swift
+//  CollectionViewMVVM
+//
+//  Created by Adriana González Martínez on 2/3/21.
+//  Copyright © 2021 Adriana González Martínez. All rights reserved.
+//
+
+import Foundation
